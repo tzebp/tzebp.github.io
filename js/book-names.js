9023e48ad7b38496a5806e394c64159c6ef0c919
tzebp <tzebp@proton.me>
2026-07-14 11:11:52 -0400

    update XXC

--- js/book-names.js
+++ js/book-names.js
@@ -1,6 +1,6 @@
 var books = [
   { name: "Asal Mulo Dunio", ref: "Aktb-01-GEN-001.html" },
   { name: "Zabór", ref: "Aktb-02-PSA-001.html" },
-  { name: "Cerito dari Taurat, Zabór, dan Énjél", ref: "Aktb-03-XXC-001.html" },
+  { name: "Cerito dari Taurat, Zabór, jugo Énjél", ref: "Aktb-03-XXC-001.html" },
   { name: "Kamus Kato Kónci", ref: "Aktb-04-GLO-001.html" },
 ];
